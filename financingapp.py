40357d23b57a5e472a119ea2106298aadcf89202
Michaela Cook <mcook132@cedarville.edu>
2025-06-26 14:33:49 -0400

    Update financingapp.py

--- financingapp.py
+++ financingapp.py
@@ -34,4 +34,7 @@ def index():
         print("Workbook saved!")
         return redirect("/")
 
-    return render_template("index.html")
+        return render_template("index.html")
+    
+    if __name__ == "__main__":
+        app.run(debug=True)
