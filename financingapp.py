40c1e76950dc5422f9f4da32441d53d88c73796c
Michaela Cook <mcook132@cedarville.edu>
2025-06-26 22:24:26 -0400

    Update financingapp.py

--- financingapp.py
+++ financingapp.py
@@ -15,7 +15,7 @@ def index():
         date = datetime.now().strftime("%m/%d/%Y")
 
         # Load styled template
-        template_path = "expenses - app.xlsx"
+        template_path = "/ExpenseWeb/expenses - app.xlsx"
         wb = openpyxl.load_workbook(template_path)
         sheet = wb["Expenses"]
         print("Using sheet:", sheet.title)
